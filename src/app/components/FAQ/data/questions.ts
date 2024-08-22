07afc73caa439ae9b6ed4a91edf764bcf8eeeeb8
Vladyslav Pobihailo <v.pobihailo@everstake.one>
2024-08-22 13:09:39 +0300

    change faq text

--- src/app/components/FAQ/data/questions.ts
+++ src/app/components/FAQ/data/questions.ts
@@ -20,39 +20,39 @@ export const questions = (coinName: string): Question[] => {
     },
     {
       title: "What does this staking dApp allow me to do?",
-      content: `<p>The staking dApp allows ${coinName} holders to stake their ${coinName} and delegate their voting power to a finality provider they select. Stakers can view their past staking history and on-demand unlock their stake for early withdrawal.</p>`,
+      content: `<p>The staking dApp is an interface to the Babylon Bitcoin Staking protocol. The Babylon Bitcoin Staking protocol allows BTC holders to stake their BTC and delegate their voting power to a finality provider they select. Stakers can view their past staking history and send a request to unlock their stake for early withdrawal.</p>`,
     },
     {
       title: `Does my ${coinName} leave my wallet once staked?`,
-      content: `<p>Yes, it leaves your wallet. Your wallet will not show it as your available balance because it is locked. However, it is not sent to any third party. It is locked in a self-custodial contract you control. This means that any subsequent movement of the ${coinName} will need your approval, and you are the only one who can unbond the stake and withdraw.</p>`,
+      content: `<p>Technically, your BTC has not left your custody. However, your wallet will not show the BTC you staked in your available balance once that BTC is locked. Current wallet implementations do not yet know how to display staked BTC that is still in your custody. When staking, you do not send the BTC to a third party. It is locked in a self-custodial Bitcoin Staking script that you control. This means that any subsequent movement of the BTC will need your approval. You are the only one who can unbond the stake and withdraw.</p>`,
     },
     {
       title: `Is my ${coinName} Safe? Could I get slashed?`,
-      content: `<p>You are not required to sign any PoS slashing-related authorizations. Thus, in theory, the ${coinName} in your self-custodial contract cannot be slashed due to the absence of your authorization.</p><br />
+      content: `<p>You are not required to sign any PoS slashing-related authorizations. Thus, in theory, the BTC in your self-custodial contract cannot be slashed due to the absence of your authorization.</p><br>
 
-      <p>However, there are still risks associated with your ${coinName}:</p><br />
+      <p>However, there are still risks associated with your BTC:</p><br>
       
       <ol>
         <li>
-          1. Code security<br />
-          There is an inherent risk that the code developed for Bitcoin staking has vulnerabilities or bugs. The Babylon team has open-sourced the code, and it is under security audits.
+          1. Code security<br>
+          There is an inherent risk that the code developed for Bitcoin Staking has vulnerabilities or bugs. The Babylon team has open-sourced the code, and it is under security audits.
         </li>
-        <br />
+        <br>
         <li>
-          2. System reliability<br />
-          The Bitcoin staking system may be slow, unavailable, or compromised, which may cause the staking service to be unavailable or compromised, potentially leading to ${coinName} not being unbindable or not withdrawable.
+          2. System reliability<br>
+          The Bitcoin Staking system may be slow, unavailable, or compromised, which may cause the staking service to be unavailable or compromised, potentially leading to BTC not being unbondable or not withdrawable.
         </li>
       </ol>
-      `,
+      <p>The Babylon Labs team has open-sourced the code which has been audited by <a href="https://docs.babylonlabs.io/assets/files/coinspect-phase1-audit.pdf" target="_blank" rel="noopener noreferrer" class="text-primary"><u>Coinspect</u></a>, <a href="https://docs.babylonlabs.io/assets/files/zellic-phase1-audit.pdf" target="_blank" rel="noopener noreferrer" class="text-primary"><u>Zellic</u></a>, and through a <a href="https://docs.babylonlabs.io/assets/files/cantina-phase1-competition.pdf" target="_blank" rel="noopener noreferrer" class="text-primary"><u>public security campaign facilitated by Cantina</u></a>.</p>`,
     },
     {
       title: "How long will it take for my stake to become active?",
-      content: `<p>A stake’s status demonstrates the current stage of the staking process. All stake starts in a Pending state which denotes that the ${coinName} Staking transaction does not yet have sufficient ${coinName} block confirmations. As soon as it receives 10 ${coinName} block confirmations, the status transitions to <i>Overflow</i> or <i>Active</i>. </p><br />
+      content: `<p>A stake’s status demonstrates the current stage of the staking process. All stake starts in a <i>Pending</i> state, which denotes that the BTC Staking transaction does not yet have sufficient BTC block confirmations. As soon as it receives 2 BTC block confirmations, the status transitions to <i>Overflow</i> or <i>Active</i>. </p><br>
       
-      <p>In an amount-based cap, A stake is <i>Overflow</i> if the system has already accumulated the maximum amount of ${coinName} it can accept.</p><br />
-      <p>In a time-based cap, where there is a starting block height and ending block height, a stake is <i>overflow</i> if it is included in a ${coinName} block that is newer than the ending block.</p><br /> 
-      <p>Otherwise, the stake is <i>Active</i>.</p><br /> 
-      <p><i>Overflow</i> stake should be on-demand unbonded and withdrawn.</p>`,
+      <p>In an amount-based cap, a stake is <i>Overflow</i> if the system has already accumulated the maximum amount of BTC it can accept.</p><br>
+      <p>In a time-based cap, where there is a starting block height and an ending block height, a stake is <i>Overflow</i> if it is included in a BTC block that is newer than the ending block.</p><br> 
+      <p>Otherwise, the stake is <i>Active</i>.</p><br> 
+      <p>You should unbond and withdraw a stake that is <i>Overflow</i>.</p>`,
     },
     {
       title: "Do I get rewards for staking?",
@@ -60,30 +60,39 @@ export const questions = (coinName: string): Question[] => {
     },
     {
       title: "Are there any other ways to stake?",
-      content: `<p>Hands-on stakers can operate the <a href="https://github.com/babylonchain/btc-staker/blob/dev/docs/create-phase1-staking.md" target="_blank" rel="noopener noreferrer" class="text-primary"><u>btc-staker CLI program</u></a> that allows for the creation of ${coinName} staking transactions from the CLI.</p>
-      `,
+      content: `<p>Hands-on stakers can operate the <a href="https://github.com/babylonlabs-io/btc-staker/blob/dev/docs/create-phase1-staking.md" target="_blank" rel="noopener noreferrer" class="text-primary"><u>btc-staker CLI program</u></a> that allows for the creation of BTC staking transactions from the CLI.</p>`,
     },
     {
       title: "Will I pay any fees for staking?",
-      content: `<p>Yes, there are three transaction fees associated with staking, all charged by the Bitcoin network:</p><br />
+      content: `<p>You will need to pay BTC network fees to have transaction messages delivered and results recorded on the Bitcoin blockchain. Examples include staking, unbonding, and withdrawal transactions. This interface may provide estimates of network fees. However, the actual network fee may be higher. <br>
+      <b>If you lock your BTC in a staking transaction without the necessary amount of BTC in the staked amount to pay for the unbonding and withdrawal transactions, you won’t be able to unbond or withdraw (although that also means the staked amount is less than those transaction fees). The stake will remain locked unless the BTC network fees come down sufficiently.</b> Here are more details about the network fees:</p><br>
       <ol>
         <li>
           1. <b>Staking Transaction Fee (Fs)</b>: This fee is for the staking transaction. To stake amount S, you need at least S + Fs in your wallet. It is calculated in real-time based on current network conditions.
-        </li><br />
+        </li><br>
         <li>
           2. <b>Unbonding Transaction Fee (Fu)</b>: If you unbond your stake before it expires, this fee is deducted from your stake S, resulting in a withdrawable amount of S - Fu. Fu is a calculated static value to ensure inclusion in busy network conditions.
-        </li><br />
+        </li><br>
         <li>
           3. <b>Withdraw Transaction Fee (Fw)</b>: This fee is for the withdrawal transaction that transfers the stake back to your wallet. It is deducted from your withdrawable stake, which is either S (if you wait until expiration) or S - Fu (if unbonded early). This fee ensures fast inclusion based on current network conditions.
         </li>
-      </ol><br />
+      </ol><br>
       <p>In summary, to stake S, you need S + Fs, and upon completion, you get S - Fw or S - Fu - Fw back, depending on whether you wait for expiration or unbond early.</p>`,
+    },
+    {
+      title:
+        "Is it ok to use a wallet holding fungible tokens built on Bitcoin (e.g. BRC-20/ARC-20/Runes)?",
+      content: `<p>No, this should be avoided. Please do not connect or use a Bitcoin wallet holding BRC-20, ARC-20, Runes, or other NFTs or Bitcoin-native assets (other than BTC). They are still in their infancy and in an experimental phase. Software built for the detection of such tokens to avoid their misspending may not work, and you may lose all such tokens.</p>`,
+    },
+    {
+      title: "Are hardware wallets supported?",
+      content: `<p>Keystone via QR code is the only hardware wallet supporting Bitcoin Staking. Using any other hardware wallet through any means (such as connection to a software/extension/mobile wallet) can lead to permanent inability to withdraw the stake.</p>`,
     },
   ];
   if (shouldDisplayTestingMsg()) {
     questionList.push({
       title: "What is the goal of this testnet?",
-      content: `<p>The goal of this testnet is to ensure the security of the staked Bitcoins by testing the user's interaction with the ${coinName} network. This will be a lock-only network without any PoS chain operating, meaning that the only participants of this testnet will be finality providers and ${coinName} stakers.</p>`,
+      content: `<p>The goal of this testnet is to ensure the security of the staked Bitcoins by testing the user's interaction with the BTC network. This will be a lock-only network without any PoS chain operating, meaning that the only participants of this testnet will be finality providers and BTC stakers.</p>`,
     });
   }
   return questionList;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,5 @@
 // shouldDisplayTestingMsg function is used to check if the application is running in testing mode or not.
 // Default to true if the environment variable is not set.
 export const shouldDisplayTestingMsg = (): boolean => {
-  return (
-    process.env.NEXT_PUBLIC_DISPLAY_TESTING_MESSAGES?.toString() !== "false"
-  );
+  return process.env.NEXT_PUBLIC_NETWORK === "signet";
 };
