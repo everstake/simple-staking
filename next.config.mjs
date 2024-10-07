a2133481176e46738bc53924132460cdb331994f
Vladyslav Pobihailo <v.pobihailo@everstake.one>
2024-10-07 18:20:12 +0300

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,37 @@ const nextConfig = {
     forceSwcTransforms: true,
   },
   poweredByHeader: false,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self';",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "geolocation=(), midi=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), fullscreen=(self), payment=()",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
